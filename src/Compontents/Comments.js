1f5e3ae5b6cb4b42e000649472e3f1a09f8da404
nikoloz.kvachantiradze17b@geolab.edu.ge <nikoloz.kvachantiradze17b@geolab.edu.ge>
2021-07-03 14:10:43 +0400

    add edit button

--- src/Compontents/Comments.js
+++ src/Compontents/Comments.js
@@ -4,7 +4,6 @@ import '.././App.css';
 export default function Comments() {
     const [Comments, setComments] = useState([]);
     const [Value, setValue] = useState('');
-    // const [CommentsEdit, setCommentsEdit] = useState(null);
     const [EditingText, setEditingText] = useState('')
     let referenc = useRef();
     let scroll = useRef();
@@ -18,28 +17,58 @@ export default function Comments() {
     let newInput = {id:Date.now() , text: Value  }
     function onSubmit(e){
         e.preventDefault()
-        
         setComments([
             ...Comments,
-            newInput,
+            newInput, 
             
         ])
         setValue('');
+        setEditingText(Value)
     }
-    useEffect(()=>{
-        scroll.current.scrollTo({
-            top: 4500,
-            behavior: 'auto'
-        },)
-    },)
+    
     useEffect(()=>{
         referenc.current.focus()
-    })
+    },[])
+    function onClick(id){
+        let edit =[ ...Comments].map(items =>{
+            if(items.title === id){
+                items.title = EditingText;
+            }
+            return items;
+            
+        } )
+        setEditingText(edit)
+    }
     return (
         <>
         <div className= 'container comments-Parent'>
             <h1 className='comments-user' >Users Comments</h1>
             <div className='childe-div' ref={scroll}>
+            <form action='/submit' className='comments-form' onSubmit={onSubmit} method='post' >
+                    <input 
+                    ref={referenc}
+                    value={Value}
+                    type='text'  
+                    className='comments-input'
+                    placeholder="Write a comment..."
+                    onChange={e => setValue(e.target.value)}
+                    />
+                    <button className='comments-btn'>add comments</button>
+                    {
+                        !!EditingText && <div><input 
+                        type='text' 
+                        onChange={e => setEditingText(e.target.value)}
+                        value={EditingText}
+                        />
+                        <button onClick={()=>{
+                            onClick(Comments.title)
+                        }}>Edit Text</button>
+                        </div>
+                       
+                    }
+                     
+                </form>
+                
                 <ul className='Comments'>
                     {
                         Comments.map(comments => (
@@ -51,25 +80,7 @@ export default function Comments() {
                         ))
                     }
                 </ul>
-                <form action='/submit' className='comments-form' onSubmit={onSubmit} method='post' >
-                    <input 
-                    ref={referenc}
-                    value={Value}
-                    type='text'  
-                    className='comments-input'
-                    placeholder="Write a comment..."
-                    onChange={e => setValue(e.target.value)}
-                    />
-                    <button className='comments-btn'>add comments</button>
-                    {
-                        !!EditingText && <input 
-                        type='text' 
-                        onChange={e => setEditingText(e.target.value)}
-                        value={EditingText}
-                        />
-                    }
-                    
-                </form>
+                
                 
            </div>
            
